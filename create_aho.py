30094c7fd892140fe995b482650d11f802235299
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2022-01-22 03:12:23 -0800

    Use Aho–Corasick to speed up frequency list stats

---
+++ create_aho.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pickle
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+from ahocorapy.keywordtree import KeywordTree
+    
+addon_directory = os.path.dirname(__file__)
+
+# Search the sentences for words in the frequency list
+freq_file_path = os.path.join(addon_directory, 'freq.txt')
+freq_file = open(freq_file_path, encoding='utf_8_sig')
+freq_data = freq_file.read().splitlines()
+
+kwtree = KeywordTree(case_insensitive=True)
+for word in freq_data:
+    kwtree.add(word)
+kwtree.finalize()  
+
+kwtree_file = open("freq-aho.pickle", "wb")
+pickle.dump(kwtree, kwtree_file)
+kwtree_file.close()
