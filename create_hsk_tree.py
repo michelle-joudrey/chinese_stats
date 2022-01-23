3acf531b4ec64bc451db51e2bb8ad8103c11ce34
Michelle Joudrey <michelle-joudrey@users.noreply.github.com>
2022-01-22 18:32:19 -0800

    Optimize HSK stats

--- create_hsk_tree.py
+++ create_hsk_tree.py
@@ -1,21 +1,22 @@
 import os
 import sys
 import pickle
+import json
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 from ahocorapy.keywordtree import KeywordTree
     
 addon_directory = os.path.dirname(__file__)
 
-freq_file_path = os.path.join(addon_directory, 'freq.txt')
-freq_file = open(freq_file_path, encoding='utf_8_sig')
-freq_data = freq_file.read().splitlines()
+hsk_file_path = os.path.join(addon_directory, 'hsk.json')
+hsk_file = open(hsk_file_path, encoding='utf_8_sig')
+hsk_data = json.load(hsk_file)
 
 kwtree = KeywordTree(case_insensitive=True)
-for word in freq_data:
+for word, _ in hsk_data.items():
     kwtree.add(word)
 kwtree.finalize()  
 
-kwtree_file = open("freq_tree.pickle", "wb")
+kwtree_file = open("hsk_tree.pickle", "wb")
 pickle.dump(kwtree, kwtree_file)
 kwtree_file.close()
